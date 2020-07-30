b270d2bb55026bb1e5295a7cc6955c0500630d2b
JosueMinchez-M <josueminchezm@gmail.com>
2020-07-29 21:17:42 -0600

    Se agregó el metodo para saber si esta vacía la lista y el metodo para poder agregar a la lista

--- src/main/java/com/mycompany/listasenlazadas/Lista.java
+++ src/main/java/com/mycompany/listasenlazadas/Lista.java
@@ -6,10 +6,24 @@ package com.mycompany.listasenlazadas;
  */
 public class Lista {
     Nodo inicio, fin; //Punteros, para acceder al inicio y al fin de la lista. Estas son referencias
-    
+    int cont = 0;
     public Lista(){
         inicio = null;
         fin = null;
     }
+    //Método que nos permite saber si la lista esta vacia o no.
+    public boolean estaVacia(){
+        return inicio == null;
+    }
     
+    //Método para agregar elementos al inicio de la lista.
+    public void agregandoAlInicio(String d){
+        cont++;
+        if(estaVacia()){
+            inicio = new Nodo(d, inicio);
+            fin = inicio;
+        }else{
+            inicio = new Nodo(d, inicio);
+        }
+    }
 }
