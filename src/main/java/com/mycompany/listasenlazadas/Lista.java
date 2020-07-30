d6d494c22d5b3e24aaba5dd9b7870e4bf8f0dc3d
JosueMinchez-M <josueminchezm@gmail.com>
2020-07-29 20:06:48 -0600

    se crean las referencias de la clase Nodo y el metodo constructor en la clase Lista

--- src/main/java/com/mycompany/listasenlazadas/Lista.java
+++ src/main/java/com/mycompany/listasenlazadas/Lista.java
@@ -5,5 +5,11 @@ package com.mycompany.listasenlazadas;
  * @author joshua
  */
 public class Lista {
+    Nodo inicio, fin; //Punteros, para acceder al inicio y al fin de la lista. Estas son referencias
+    
+    public Lista(){
+        inicio = null;
+        fin = null;
+    }
     
 }
