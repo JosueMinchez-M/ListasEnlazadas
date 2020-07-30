b1bb500e6823051d54c1209a8cf29297bb613020
JosueMinchez-M <josueminchezm@gmail.com>
2020-07-29 21:28:32 -0600

    Se complementa la funcionalidad en la clase principal (Main) y se termina la logica funcional por consola

--- src/main/java/com/mycompany/listasenlazadas/Principal.java
+++ src/main/java/com/mycompany/listasenlazadas/Principal.java
@@ -10,7 +10,23 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Lista miLista = new Lista();
+        miLista.agregandoAlInicio("Juan");
+        miLista.agregandoAlInicio("Ana");
+        miLista.agregandoAlInicio("Lupita");
+        miLista.agregandoAlInicio("María");
+        miLista.agregandoAlInicio("Mariano");
+        miLista.agregandoAlInicio("Javier");
+        miLista.agregandoAlInicio("Anelisse");
+        miLista.mostrarLista();
+        
+        if(miLista.estaVacia()){
+            System.out.println("La lista está vacía");
+        }else{
+            System.out.println("  La lista no está vacía");
+        }
+        
+        System.out.println("Cantidad de elementos de la lista enlazada: " + miLista.cantidadNodos());
     }
     
 }
